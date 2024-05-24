42a87823fa8b5edb3415181cae0f7cdeaa9ce54d
Bouillaguet Quentin <git@desplandis.fr>
2024-05-24 09:21:35 +0200

    fix: simplify babel conf on geodesy package

--- packages/Geodesy/src/GeoidGrid.js
+++ packages/Geodesy/src/GeoidGrid.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import Coordinates from 'Coordinates';
-import CRS from 'Crs';
+import Coordinates from './Coordinates';
+import CRS from './Crs';
 
 
 const coord = new Coordinates('EPSG:4326');
