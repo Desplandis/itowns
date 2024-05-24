17e5792028b927852a3a603addf5d8c91b0dcbf3
Bouillaguet Quentin <git@desplandis.fr>
2024-05-24 10:03:56 +0200

    todo: itowns.js build works (need to edit .babelrc for genericity)

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,51 +1,32 @@
-const fs = require('fs');
 const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 const mode = process.env.NODE_ENV;
-const noInline = process.env.noInline;
 const debugBuild = mode === 'development';
-
-/*
-   configuring babel:
-   - when babel runs alone (for `test-unit` for instance), we let him deal with
-   ES6 modules, because node doesn't support them yet (planned for v10 lts).
-   - however, webpack also has ES6 module support and these 2 don't play well
-   together. When running webpack (either `build` or `start` script), we prefer
-   to rely on webpack loaders (much more powerful and gives more possibilities),
-   so let's disable modules for babel here.
-   - we also dynamise the value of __DEBUG__ according to the env var
-*/
-// Note that we don't support .babelrc in parent folders
-const babelrc = fs.readFileSync(path.resolve(__dirname, '.babelrc'));
-const babelConf = JSON.parse(babelrc);
-
-babelConf.babelrc = false; // disabel babelrc reading, as we've just done it
-const replacementPluginConf = babelConf.plugins.find(plugin => Array.isArray(plugin) && plugin[0] === 'minify-replace');
-replacementPluginConf[1].replacements.find(decl => decl.identifierName === '__DEBUG__').replacement.value = debugBuild;
 
 const include = [
     path.resolve(__dirname, 'src'),
     path.resolve(__dirname, 'test'),
     path.resolve(__dirname, 'utils'),
+    path.resolve(__dirname, 'packages/Geodesy'),
 ];
 
-module.exports = () => {
-    const babelLoaderOptions = [];
-    if (!noInline) {
-        babelLoaderOptions.push('babel-inline-import-loader');
-    }
-    babelLoaderOptions.push({
-        loader: 'babel-loader',
-        options: babelConf,
-    });
+const exclude = [
+    path.resolve(__dirname, '.git'),
+    path.resolve(__dirname, 'node_modules'),
+];
 
+const alias = {
+    '@itowns/geodesy': path.resolve(__dirname, 'packages/Geodesy/src/Main.js'),
+};
+
+module.exports = () => {
+    console.log('TO REMOVE');
     return {
         mode,
         context: path.resolve(__dirname),
         resolve: {
-            exportsFields: ['itowns_exports', 'exports'],
-            modules: [path.resolve(__dirname, 'src'), path.resolve(__dirname, './packages/Geodesy/src'), 'node_modules'],
+            alias,
         },
         entry: {
             itowns: [
@@ -73,16 +54,18 @@ module.exports = () => {
             rules: [
                 {
                     test: /\.js$/,
-                    exclude: path.resolve(__dirname, '/node_modules/'),
+                    exclude,
                     include,
-                    use: babelLoaderOptions,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            rootMode: 'upward',
+                        },
+                    },
                 },
             ],
         },
         plugins: [
-            new ESLintPlugin({
-                files: include,
-            }),
         ],
         devServer: {
             devMiddleware: {
@@ -91,7 +74,7 @@ module.exports = () => {
             static: {
                 directory: path.resolve(__dirname, './'),
                 watch: {
-                    ignored: [path.resolve(__dirname, '.git'), path.resolve(__dirname, 'node_modules')],
+                    ignored: exclude,
                 },
             },
             client: {
